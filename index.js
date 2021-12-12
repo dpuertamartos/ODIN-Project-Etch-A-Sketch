42722cf02e1b9933b0b2c7b891d183ed54e86ac1
David Puerta Martos <dpuerta@usal.es>
2021-12-12 17:26:34 +0100

    hover coloring added and reset button added with reset functionality

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 // Create a webpage with a 16x16 grid of square divs.
 
+
+
 const container = document.querySelector(".gridContainer")
 
-for(let i=0; i<16; i++){
-    let secondContainer = document.createElement("div")
-    secondContainer.id = i+1
-    secondContainer.className = "rowContainer"
-    container.appendChild(secondContainer)
-    for(let j=0; j<16; j++){
-        let thirdContainer = document.createElement("div")
-        thirdContainer.id = `${i+1}-${j+1}`
-        thirdContainer.className = "pixel"
-        secondContainer.append(thirdContainer)
+const createtGrid = () => {
+    for(let i=0; i<16; i++){
+        let secondContainer = document.createElement("div")
+        secondContainer.id = i+1
+        secondContainer.className = "rowContainer"
+        container.appendChild(secondContainer)
+        for(let j=0; j<16; j++){
+            let thirdContainer = document.createElement("div")
+            thirdContainer.id = `${i+1}-${j+1}`
+            thirdContainer.className = "pixel"
+            secondContainer.append(thirdContainer)
+        }
     }
 }
 
+createtGrid()
 
 /* Set up a “hover” effect so that the grid divs change color when your mouse passes 
 over them, leaving a (pixelated) trail through your grid like a pen would. */
 
+const pixels = document.querySelectorAll(".pixel")
+pixels.forEach(pixel => pixel.addEventListener("mouseenter", ()=>pixel.className="pixel-hovered"))
 
 
 /* Add a button to the top of the screen which will clear the current grid and send 
@@ -27,6 +34,11 @@ Once entered, the new grid should be generated in the same total space as before
 so that you’ve got a new sketch pad. Tip: Set the limit for the user input to a maximum of 100. 
 A larger number of squares results in more computer resources 
 being used, potentially causing delays, freezing, or crashing that we want to prevent. */
+
+const resetButton = document.querySelector(".resetButton")
+resetButton.addEventListener("click", ()=>{
+    pixels.forEach(pixel => pixel.className="pixel")
+})
 
 
 

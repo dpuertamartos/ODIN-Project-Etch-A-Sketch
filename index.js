e3b278b74e205b4adbee504ab76dc023bf847c03
David Puerta Martos <dpuerta@usal.es>
2021-12-12 18:54:07 +0100

    CODE WITH OPTIONAL RESTRUCTURED

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+const maxSideSize = 960
+
 const randomizeRgb = () => Math.floor(Math.random()*256)
+
+const pixelListener = (pixel) => {
+    // If attribute hovers is 0, create a new random RGB background-color
+    if(pixel.getAttribute('hovers')==="0"){
+        const randomRgb =`rgb(${randomizeRgb()},${randomizeRgb()},${randomizeRgb()})`
+        pixel.style.backgroundColor=randomRgb
+    }
+    // If pixel was already colored, conver the RGB string into array of numbers with 25.5 decreased
+    // Use that array to create a new rgb styling. This way after 10 hovers the color will be 0,0,0-BLACK
+    
+    else{
+        const rgbToChange = pixel.style.backgroundColor
+        let arrayRGB = rgbToChange.split("(")[1].split(")")[0].split(", ").map(n=>(Number(n)-25.5))
+        pixel.style.backgroundColor=`rgb(${arrayRGB[0]},${arrayRGB[1]},${arrayRGB[2]})`
+    }
+    // Finally increase hover counter every mouseover
+    pixel.setAttribute('hovers', `${Number(pixel.getAttribute('hovers'))+1}`)
+}
 
 const createtGrid = (a=16) => {
     if(a>100){return reset()}
@@ -12,25 +32,15 @@ const createtGrid = (a=16) => {
             let thirdContainer = document.createElement("div")
             thirdContainer.id = `${i+1}-${j+1}`
             thirdContainer.className = "pixel"
-            thirdContainer.style.width = 960/a
-            thirdContainer.style.height = 960/a
+            thirdContainer.style.width = maxSideSize/a
+            thirdContainer.style.height = maxSideSize/a
+            // Created attribute hovers that will increase everytime you hover the pixel
             thirdContainer.setAttribute('hovers','0')
             secondContainer.append(thirdContainer)
         }
     }
     const pixels = document.querySelectorAll(".pixel")
-    pixels.forEach(pixel => pixel.addEventListener("mouseenter", ()=>{
-        if(pixel.getAttribute('hovers')==="0"){
-            const randomRgb =`rgb(${randomizeRgb()},${randomizeRgb()},${randomizeRgb()})`
-            pixel.style.backgroundColor=randomRgb
-        }
-        else{
-            const rgbToChange = pixel.style.backgroundColor
-            let arrayRGB = rgbToChange.split("(")[1].split(")")[0].split(", ").map(n=>(Number(n)-25.5))
-            pixel.style.backgroundColor=`rgb(${arrayRGB[0]},${arrayRGB[1]},${arrayRGB[2]})`
-        }
-        pixel.setAttribute('hovers', `${Number(pixel.getAttribute('hovers'))+1}`)
-    }))
+    pixels.forEach(pixel => pixel.addEventListener("mouseenter", ()=> pixelListener(pixel)))
 }
 
 const destroyGrid = () => {
@@ -48,19 +58,11 @@ const reset = () =>{
     createtGrid(n)
 }
 
-
 const resetButton = document.querySelector(".resetButton")
 resetButton.addEventListener("click", ()=>reset())
 
 const destroyButton = document.querySelector(".destroyButton")
 destroyButton.addEventListener("click", ()=>destroyGrid())
 
-
 createtGrid()
 
-
-
-/* (Optional): Instead of just changing the color of a square from black to white (for example), 
-have each pass through with the mouse change it to a completely random RGB value. 
-Then try having each pass just add another 
-10% of black to it so that only after 10 passes is the square completely black. */
